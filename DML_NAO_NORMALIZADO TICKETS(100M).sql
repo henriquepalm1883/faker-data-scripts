0e51829e1db4ab8a14064b28c4534260483e27f0
henriquepalm1883 <henrique.palm@universo.univates.br>
2025-04-01 17:57:59 -0300

    Add files via upload

---
+++ DML_NAO_NORMALIZADO TICKETS(100M).sql
@@ -0,0 +1,7 @@
+INSERT INTO ticket (data, tipo_veiculo_idvalores, valor_ocorrencia, praca_id_praca)
+SELECT
+    NOW() - (RANDOM() * INTERVAL '450 days'),  
+    (SELECT idvalores FROM tipo_veiculo ORDER BY RANDOM() LIMIT 1),  
+    ROUND((RANDOM() * (20.00 - 5.00) + 5.00)::numeric, 2),  
+    (SELECT id_praca FROM praca ORDER BY RANDOM() LIMIT 1)  
+FROM generate_series(1, 100000000);
