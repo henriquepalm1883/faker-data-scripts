f91b0362b89ae9e5eef10c0872903387dd1e9edd
henriquepalm1883 <henrique.palm@universo.univates.br>
2025-04-06 20:40:12 -0300

    Delete DML_NORMALIZADO TICKETS(10M).sql

--- DML_NORMALIZADO TICKETS(10M).sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO ticket (data, tipo_veiculo_idvalores, valor_ocorrencia, praca_id_praca)
-SELECT
-    NOW() - (RANDOM() * INTERVAL '365 days'),  
-    (SELECT idvalores FROM tipo_veiculo ORDER BY RANDOM() LIMIT 1),  
-    ROUND((RANDOM() * (99.99 - 10.00) + 10.00)::numeric, 2),  
-    (SELECT id_praca FROM praca ORDER BY RANDOM() LIMIT 1)  
-FROM generate_series(1, 10000000);
